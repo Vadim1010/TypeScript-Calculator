31cc45fada9637b02a1fcd0e1645b3f580faee12
Vadim <vv342915007@gmail.com>
2017-09-25 05:56:52 +0300

    corrected the mistake second

--- src/app/calculator/events/calculatorResult.ts
+++ src/app/calculator/events/calculatorResult.ts
@@ -1,5 +1,13 @@
+import {value} from "../constanta";
+
 export function calculatorResult () {
-    let result:number = +document.getElementsByClassName('btn--result')[0].innerHTML;
+    let result: number = +document.getElementsByClassName('btn--result')[0].innerHTML,
+        resultObj = document.getElementsByClassName('btn--result')[0],
+        value:string;
 
     result *=  result;
+
+    value = result + "";
+
+    resultObj.innerHTML = value;
 }
